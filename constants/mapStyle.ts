9d8f179289341c959c549c14ba704d74a070dc32
annie <mbgkzw@gmail.com>
2026-09-08 21:04:04 +0400

    fix case collision soft map

--- constants/mapStyle.ts
+++ constants/mapStyle.ts
@@ -1,18 +1,19 @@
 export const softMapStyle = [
   { elementType: "geometry", stylers: [{ color: "#f5f3ef" }] },
   { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-  { elementType: "labels.text.fill", stylers: [{ color: "#9a9a9a" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#a8a8a8" }] },
   { elementType: "labels.text.stroke", stylers: [{ color: "#f5f3ef" }] },
   { featureType: "poi", stylers: [{ visibility: "off" }] },
-  { featureType: "poi.park", elementType: "geometry", stylers: [{ color: "#e5f3e8", visibility: "on" }] },
+  { featureType: "poi.business", stylers: [{ visibility: "off" }] },
+  { featureType: "poi.park", elementType: "geometry", stylers: [{ color: "#e5f3e8" }] },
   { featureType: "road", elementType: "geometry", stylers: [{ color: "#ffffff" }] },
   { featureType: "transit", stylers: [{ visibility: "off" }] },
   { featureType: "water", elementType: "geometry", stylers: [{ color: "#d6eaf8" }] },
 ]
 
-export const repairColors = {
-  roadwork: '#FFD6A5',
-  accident: '#FFB3BA',
-  closed: '#D5C4F5',
-  default: '#C7CEEA'
+export const repairColors:any={
+  roadwork:'#FFD6A5',
+  accident:'#FFB3BA',
+  closed:'#D5C4F5',
+  default:'#C7CEEA'
 }
